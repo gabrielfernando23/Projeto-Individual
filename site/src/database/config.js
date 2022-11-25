83388c072bfe1679210c54d3131ba91393ecda0d
gabrielfernando23 <gabriel.ssilva@sptech.school>
2022-11-25 08:07:08 -0300

    att

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_DATABASE",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "scores23",
+    user: "usuario",
+    password: "2301",
 };
 
 function executar(instrucao) {
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -16,6 +16,11 @@ router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
 
+router.post("/cadastrarTimes", function (req, res) {
+    usuarioController.cadastrarTimes(req, res);
+})
+
+
 router.post("/autenticar", function (req, res) {
     usuarioController.entrar(req, res);
 });
